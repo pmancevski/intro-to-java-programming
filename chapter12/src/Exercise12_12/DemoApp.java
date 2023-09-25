4206a2471de65a7e4168fc5e6d4792da30b67f68
pmancevski <pmancevskiwork@gmail.com>
2023-09-25 20:26:24 +0200

    update

--- chapter12/src/Exercise12_12/DemoApp.java
+++ chapter12/src/Exercise12_12/DemoApp.java
@@ -1,44 +1,34 @@
-package Exercise12_11;
+package Exercise12_12;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
-/*
-    The exercise is modified to be run from IDE (Intellij) and input from console.
- */
 public class DemoApp {
     public static void main(String[] args) {
-
-//        if (args.length != 2){
-//            System.out.println("Usage: java Exercise12_11 wordToBeReplaced filename");
-//            System.exit(-1);
-//        }
-
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter word to be removed: ");
-        String wordToBeRemoved = input.next();
 
         System.out.print("Enter file name: ");
         String fileName = input.next();
 
         File file = new File(fileName);
 
-        String string = "";
+        String content = "";
 
         try (Scanner inputFromFile = new Scanner(file)){
             while (inputFromFile.hasNextLine()){
-                string += inputFromFile.nextLine().replace((wordToBeRemoved + " "), "");
-                string += "\n";
+                content += inputFromFile.nextLine();
             }
         }
         catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
 
-        try (PrintWriter output = new PrintWriter(file)){
-            output.println(string);
+        //content = content.replace("\n{", "{\n");
+
+        try (PrintWriter output = new PrintWriter(fileName)){
+            output.print(content);
         }
         catch (FileNotFoundException e) {
             throw new RuntimeException(e);
