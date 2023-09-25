b32a6a2ba4b55bc5dac9d9ac16b6319bc0ba1a91
pmancevski <pmancevskiwork@gmail.com>
2023-09-25 20:02:27 +0200

    update

--- chapter12/src/Exercise12_11/DemoApp.java
+++ chapter12/src/Exercise12_11/DemoApp.java
@@ -1,7 +1,31 @@
 package Exercise12_11;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class DemoApp {
     public static void main(String[] args) {
 
+        File file = new File("test.txt");
+
+        String string = "";
+
+        try (Scanner input = new Scanner(file)){
+            while (input.hasNextLine()){
+                string = input.nextLine();
+            }
+        }
+        catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        try (PrintWriter output = new PrintWriter(file)){
+            output.println(string);
+        }
+        catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
